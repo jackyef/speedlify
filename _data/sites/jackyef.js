db17678914d969790b666f56017edc17c7c2f88c
jackyef <jacky.efendi1@gmail.com>
2022-01-16 15:57:51 +0700

    Do not skip

--- _data/sites/jackyef.js
+++ _data/sites/jackyef.js
@@ -2,8 +2,7 @@ module.exports = {
 	name: "jackyef.com", // optional, falls back to object key
 	description: "Jacky’s Personal web site",
 	// skip if localhost
-	// skip if this is a new fork of the speedlify (not Zach’s)
-	skip: !process.env.CONTEXT || process.env.SITE_NAME !== "speedlify",
+	skip: !process.env.CONTEXT,
 	options: {
 		frequency: 60 * 23, // 23 hours
 		// Use "run" if the sites don’t share assets on the same origin
@@ -13,8 +12,8 @@ module.exports = {
 		freshChrome: "site"
 	},
 	urls: [
-		"https://www.zachleat.com/",
-		"https://www.zachleat.com/about",
-		"https://www.zachleat.com/blog",
+		"https://jackyef.com/",
+		"https://jackyef.com/about",
+		"https://jackyef.com/blog",
 	]
 };
